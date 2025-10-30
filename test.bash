777e321df43a3cf6ba308e49cf160e9fbdeff6eb
HAR <har@hurusagishi.com>
2025-10-30 12:55:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -6,8 +6,9 @@ ng () {
 }
 
 res=0
-a=春川
-[ "$a" = 秋川 ] || ng "$LINENO"
-[ "$a" = 春川 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
